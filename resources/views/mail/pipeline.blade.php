b72372defddaca4ef79ade9bd02fdb377cc9d6a4
JGomes <jose.gomes@olx.com>
2024-02-02 00:31:52 +0000

    WIP pipeline improvements

--- resources/views/mail/pipeline.blade.php
+++ resources/views/mail/pipeline.blade.php
@@ -41,6 +41,6 @@
     <body>
         <h1>Jenkins notification</h1>
         <p><span class="result">{{ $result }}</span></p>
-        <pre>{!! nl2br($msg) !!}</pre>
+        <pre>{!! str_replace("\n", '<br>', $msg) !!}</pre>
     </body>
 </html>
