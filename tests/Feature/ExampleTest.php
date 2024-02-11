aa1f9e08cfc6283515efc4c4a51243ec474b2af9
JGomes <jose.gomes@olx.com>
2024-02-11 12:45:57 +0000

    Improvements on the ready to dev scrip

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,7 @@ class ExampleTest extends TestCase
     public function test_example()
     {
         $response = $this->get('/');
+        $response->assertOk();
         //$response->assertStatus(200);
     }
 }
