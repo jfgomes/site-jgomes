1af398f28a67a7fd5e3d4cba97b193f81202f8c3
jgomes2 <jgomes.site@gmail.com>
2024-02-12 16:17:07 +0000

    test git flow

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <!--- Basic Page Needs
     ================================================== -->
     <meta charset="utf-8">
-    <title>José Gomes</title>
+    <title>José Gomes test</title>
 
     <meta name="description" content="">
     <meta name="author" content="">
