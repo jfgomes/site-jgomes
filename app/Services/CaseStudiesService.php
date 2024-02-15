8aed7666513324461aa8a72bcbcd08672334100e
JGomes <jose.gomes@olx.com>
2024-02-15 17:50:05 +0000

    Improve docs

--- app/Services/CaseStudiesService.php
+++ app/Services/CaseStudiesService.php
@@ -39,22 +39,24 @@ class CaseStudiesService
     {
         return match ($name)
         {
+            "0setup"              => "⚙️ Prod env Project - complete infra setup",
             "1basic-setup"        => "💻 Local env setup AKA Ready to Dev! ##DONE##",
-            "2git-rule"           => "🧩 GitHub protection rule for master branch ##DONE##",
+            "2.1ci-cd"            => "🌤️ Project CI/CD with jenkins",
+            "2.2git-rule"         => "🧩 GitHub protection rule for master branch ##DONE##",
+            "2.3wms"              => "📩 Website message service",
+            "2.4gc-bucket"        => "☁️ Do backups to a bucket using google cloud",
             "3hide-routes-cookie" => "🙈 Hide routes in production ##DONE##",
             "4maintenance-mode"   => "🏗️ Site down in maintenance mode accessible only for devs",
             "5env_vars_without"   => "🙈 Hide env vars",
             "6webpack-mix"        => "🧑‍💻 Client browser cache - how to handlee",
             "7rate-limit"         => "👮‍♀️Requests limit per route - rate limit security",
             "8logging"            => "🗂️ Laravel log files",
-            "9error-pages"        => "📄 Laravel custom error views",
-            "wms"          => "Website message service",
-            "test"         => "Cache implementation",
-            "test2"        => "Elasticsearch implementation",
-
-            "setup"        => "Project complete infra setup",
-            "cicd"         => "Project ci/cd with jenkins",
-
+            "9.0error-pages"      => "📄 Laravel custom error views",
+            "9.0.1redis"          => "💿 Redis implementation",
+            "9.1swagger"          => "🕹️ Swagger implementation",
+            "9.2reboot-cron"      => "⚙️ Some jobs in prod",
+            "9.3unit-tests"       => "🧪 Phpunit tests",
+            "9.4es"               => "🔎 Elasticsearch implementation",
             default => $name,
         };
     }
