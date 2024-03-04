7af9e0fa68113901bd8996126a2212343b499a29
JGomes <jose.gomes@olx.com>
2024-03-04 13:25:10 +0000

    server less auth WIP

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,15 +3,29 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Login Form</title>
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <title>Login</title>
+        <link rel="stylesheet" href="css/local/private/loadingOverlay.css">
+        <link rel="stylesheet" href="css/local/private/flashMessages.css">
+        <script src="js/jquery-1.10.2.min.js"></script>
         <script src="{{ (app()->environment() === 'prod')
             ? mix('js/prod/private/app.js')
             : 'js/local/private/serverLessRequests.js' }}">
         </script>
     </head>
-    <body>
-        <div id="response-message"></div>
+    <body id="login-body">
+        <div id="overlay">
+            <div id="overlay-content"> ⏳</div>
+        </div>
+        @if(request()->has('error'))
+            <div class="flash-message error">
+                {{ request()->get('error') }}
+            </div>
+        @endif
+        @if(request()->has('success'))
+            <div class="flash-message success">
+                {{ request()->get('success') }}
+            </div>
+        @endif
         <h2>Login</h2>
         <form id="login-form">
             <div>
@@ -21,11 +35,17 @@
             <div>
                 <label for="password">Password:</label><br>
                 <input type="password" id="password" name="password" required>
-            </div>
-            <br>
+            </div><br>
             <button id="login-btn" type="button">
                 Login
             </button>
         </form>
     </body>
+    <script>
+        // Initialize the module when the DOM is ready.
+        $(document).ready(function()
+        {
+            serverLessRequests.init();
+        });
+    </script>
 </html>
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,23 +4,24 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Home</title>
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <link rel="stylesheet" href="css/local/private/loadingOverlay.css">
+        <link rel="stylesheet" href="css/local/private/flashMessages.css">
+        <script src="js/jquery-1.10.2.min.js"></script>
         <script src="{{ (app()->environment() === 'prod')
             ? mix('js/prod/private/app.js')
             : 'js/local/private/serverLessRequests.js' }}">
         </script>
     </head>
     <body>
+        <div id="overlay">
+            <div id="overlay-content"> ⏳</div>
+        </div>
         <h2>Home</h2>
-        <button id="logout-btn">
+        <button id="logout-btn" onclick="serverLessRequests.doLogout()">
             Logout
         </button>
         <script>
-            // To avoid infinitive loop.. too many requests.. let's exclude login page..
-            if (window.location.pathname !== "/login")
-            {
-                serverLessRequests.checkAuthAndGetData('/api/v1/home');
-            }
+            serverLessRequests.checkAuthAndGetData('/api/v1/home');
         </script>
     </body>
 </html>
