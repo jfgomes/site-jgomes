54f1cd729573fa7d77c2e1b55f5b9c0485d1a8ab
Not code owner <159710158+jfgomes2@users.noreply.github.com>
2024-03-20 20:54:19 +0000

    Fix1 (#102)
    * test
    
    * test
    
    * test
    
    ---------
     
    Co-authored-by: JGomes <zx.gomes@gmail.com>

--- resources/views/locations/index.blade.php
+++ resources/views/locations/index.blade.php
@@ -56,15 +56,16 @@
                     <div id="map"></div>
                 </div>
                 <div class="side-panel">
-                    <div class="backInRight buttons-container" style="min-height: 36px;">
-                        @if ($cookieExists || app()->environment('local'))
+
+                    <div class="backInRight buttons-container" style="min-height: 36px">
+
                             <button id="resetRedisCache" class="custom-btn">
                                 Clean Redis
                             </button>
                             <button id="resetAPCuCache" class="custom-btn">
                                 Clean APCu
                             </button>
-                        @endif
+
                     </div>
                     <div class="logs-container">
                         <label for="loadLogs"></label>
