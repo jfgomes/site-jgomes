27d06cc8d9e2e1670793393980f42b604fa26fd5
JGomes <jose.gomes@olx.com>
2024-06-02 22:32:28 +0100

    optimize searches

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -20,7 +20,6 @@ class SearchController extends Controller
         $username = env('ELASTICSEARCH_USERNAME');
         $password = env('ELASTICSEARCH_PASSWORD');
 
-
         $this->elasticsearch = ClientBuilder::create()
             ->setHosts(["$host:$port"])
             ->setBasicAuthentication($username, $password)
@@ -43,7 +42,15 @@ class SearchController extends Controller
                     'body' => [
                         'settings' => [
                             'number_of_shards' => 1,
-                            'number_of_replicas' => 0
+                            'number_of_replicas' => 0,
+                            'analysis' => [
+                                'analyzer' => [
+                                    'email_analyzer' => [
+                                        'type' => 'custom',
+                                        'tokenizer' => 'uax_url_email'
+                                    ]
+                                ]
+                            ]
                         ],
                         'mappings' => [
                             'properties' => [
@@ -51,7 +58,8 @@ class SearchController extends Controller
                                     'type' => 'text'
                                 ],
                                 'email' => [
-                                    'type' => 'text'
+                                    'type' => 'text',
+                                    'analyzer' => 'email_analyzer'
                                 ],
                                 'role' => [
                                     'type' => 'text'
@@ -68,45 +76,61 @@ class SearchController extends Controller
                 ];
 
                 $this->elasticsearch->indices()->create($params);
-
             }
         } catch (\Exception $e) {
             throw new \RuntimeException('Erro ao verificar ou criar o índice: ' . $e->getMessage());
         }
     }
 
-
     public function search(Request $request): \Illuminate\Http\JsonResponse
     {
         try {
+            // Validação para garantir que 'query' não é null
+            $request->validate([
+                'query' => 'required|string'
+            ]);
 
-        // Validação para garantir que 'query' não é null
-        $request->validate([
-            'query' => 'required|string'
-        ]);
-
-        $query = $request->input('query');
+            $query = $request->input('query');
 
             // Verificar se o índice existe e criar se não existir
             $this->ensureIndexExists('users');
 
+            // Definir a consulta
             $params = [
-            'index' => 'users',
-            'body'  => [
-                'query' => [
-                    'multi_match' => [
-                        'query'  => $query,
-                        'fields' => ['title', 'email', 'role']
+                'index' => 'users',
+                'body'  => [
+                    'query' => [
+                        'bool' => [
+                            'should' => [
+                                [
+                                    'match_phrase' => [
+                                        'email' => $query
+                                    ]
+                                ],
+                                [
+                                    'match_phrase' => [
+                                        'name' => $query
+                                    ]
+                                ],
+                                [
+                                    'multi_match' => [
+                                        'query'  => $query,
+                                        'fields' => ['role']
+                                    ]
+                                ]
+                            ]
+                        ]
                     ]
                 ]
-            ]
-        ];
+            ];
 
-
+            // Realizar a pesquisa
             $results = $this->elasticsearch->search($params);
             return response()->json($results->asArray());
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
+
+
 }
